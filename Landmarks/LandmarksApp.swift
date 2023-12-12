103990814ae5c7df803416fa945dd22de6a2c0e3
Burak Ozdemir <ozdemiirburak@gmail.com>
2023-12-12 11:05:01 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Burak Özdemir on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
